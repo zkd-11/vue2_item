f8d46b869e4f2d564da41c24a2334a69a537792c
zkd-11 <1137756821@qq.com>
2023-06-21 20:24:34 +0800

    up9_icon

--- travel/travel_item/src/Pages/components/Swiper.vue
+++ travel/travel_item/src/Pages/components/Swiper.vue
@@ -1,4 +1,3 @@
-<!-- eslint-disable vue/valid-v-for -->
 <template>
   <div class="wrapper">
     <!--
--- travel/travel_item/src/Pages/home.vue
+++ travel/travel_item/src/Pages/home.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
-    <home-header>Home</home-header>
+    <home-header></home-header>
     <home-swiper></home-swiper>
-    <h1>11111111</h1>
+    <home-icons></home-icons>
   </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
